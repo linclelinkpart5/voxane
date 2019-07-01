72670d87a06f48f200556c302aec768902c74084
Mark LeMoine <thatsgobbles@gmail.com>
2019-06-30 21:50:12 -0700

    Simple starting `Error` class

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Clone, Copy, Debug)]
+pub enum Error {
+    ZeroBands,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::ZeroBands => write!(f, "number of bands must be greater than zero"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            _ => None,
+        }
     }
 }
