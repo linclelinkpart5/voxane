885129460659b687955c6b6bcee8fe59feb925c9
Mark LeMoine <thatsgobbles@gmail.com>
2019-07-05 16:44:06 -0700

    Updating to cpal 0.10

--- src/audio/sound_card.rs
+++ src/audio/sound_card.rs
@@ -6,19 +6,39 @@ use cpal::EventLoop;
 use cpal::UnknownTypeInputBuffer;
 use cpal::StreamData;
 use cpal::Sample;
+use cpal::traits::HostTrait;
+use cpal::traits::DeviceTrait;
+use cpal::traits::EventLoopTrait;
 
-pub struct SoundCard;
+pub struct SoundCard {
+    event_loop: EventLoop,
+}
 
 impl SoundCard {
+    pub fn new() -> Self {
+        // Setup the default input device and stream with the default input format.
+        let host = cpal::default_host();
+
+        let event_loop = host.event_loop();
+
+        let device = host.default_input_device().expect("failed to get default input device");
+        let format = device.default_input_format().expect("failed to get default input format");
+        let stream_id = event_loop.build_input_stream(&device, &format).expect("failed to build input stream");
+        event_loop.play_stream(stream_id);
+
+        Self {
+            event_loop
+        }
+    }
+
     pub fn run() {
         // Setup the default input device and stream with the default input format.
-        let device = cpal::default_input_device().expect("failed to get default input device");
+        let host = cpal::default_host();
+
+        let event_loop = host.event_loop();
+
+        let device = host.default_input_device().expect("failed to get default input device");
         let format = device.default_input_format().expect("failed to get default input format");
-
-        println!("Default input device: {}", device.name());
-        println!("Default input format: {:?}", format);
-
-        let event_loop = EventLoop::new();
         let stream_id = event_loop.build_input_stream(&device, &format).expect("failed to build input stream");
         event_loop.play_stream(stream_id);
 
@@ -30,24 +50,37 @@ impl SoundCard {
         let recording_2 = recording.clone();
 
         std::thread::spawn(move || {
-            event_loop.run(move |_id, data| {
+            event_loop.run(move |stream_id, stream_result| {
                 // If we're done recording, return early.
                 if !recording_2.load(AtomicOrdering::Relaxed) {
                     return;
                 }
+
+                let stream_data = match stream_result {
+                    Ok(data) => data,
+                    Err(err) => {
+                        eprintln!("an error occurred on stream {:?}: {}", stream_id, err);
+                        return;
+                    }
+                    _ => return,
+                };
+
                 // Otherwise write to the wav writer.
-                match data {
+                match stream_data {
                     StreamData::Input { buffer: UnknownTypeInputBuffer::U16(buffer) } => {
+                        println!("Buffer size: {}", buffer.len());
                         for sample in buffer.iter() {
                             let sample = Sample::to_f32(sample);
                         }
                     },
                     StreamData::Input { buffer: UnknownTypeInputBuffer::I16(buffer) } => {
+                        println!("Buffer size: {}", buffer.len());
                         for &sample in buffer.iter() {
                             let sample = Sample::to_f32(&sample);
                         }
                     },
                     StreamData::Input { buffer: UnknownTypeInputBuffer::F32(buffer) } => {
+                        println!("Buffer size: {}", buffer.len());
                         for &sample in buffer.iter() {
                             let sample = Sample::to_f32(&sample);
                         }
@@ -56,5 +89,17 @@ impl SoundCard {
                 }
             });
         });
+
+        loop {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SoundCard;
+
+    #[test]
+    fn test_run() {
+        SoundCard::run();
     }
 }
