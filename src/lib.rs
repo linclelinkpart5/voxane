21c6bb2cd6b4bc4005585f07bce159736ea03ca8
Mark LeMoine <thatsgobbles@gmail.com>
2019-07-14 01:42:16 -0700

    Cleaning up migrated modules, outdated `Error` variants, and all warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,22 @@
 #[cfg(test)] #[macro_use] extern crate assert_approx_eq;
 
-pub mod assign;
-pub mod audio;
 pub mod types;
 pub mod buckets;
 pub mod analyzer;
 pub mod window_kind;
 pub mod wave;
 pub mod sample;
-pub mod fft_engine;
 pub mod listener;
 #[cfg(test)] pub mod test_util;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Error {
-    InvalidNumBands,
-    InvalidLowerCutoff,
-    InvalidUpperCutoff,
-    OutOfOrderCutoffs,
-    InvalidSamplingRate,
-    NotEnoughSamples,
-    UnexpectedInputBufferSize(usize, usize),
-    UnexpectedOutputBufferSize(usize, usize),
+    NumBands,
+    LowerCutoff,
+    UpperCutoff,
+    CutoffOrder,
+    InputBuffer(usize, usize),
+    OutputBuffer(usize, usize),
     NumSamples(usize, usize),
     SamplingRate(usize),
 }
@@ -29,14 +24,12 @@ pub enum Error {
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
-            Error::InvalidNumBands => write!(f, "number of bands must be greater than zero"),
-            Error::InvalidLowerCutoff => write!(f, "lower cutoff must be greater than zero"),
-            Error::InvalidUpperCutoff => write!(f, "upper cutoff must be greater than zero"),
-            Error::OutOfOrderCutoffs => write!(f, "lower cutoff must be less than upper cutoff"),
-            Error::InvalidSamplingRate => write!(f, "sampling rate must be greater than zero"),
-            Error::NotEnoughSamples => write!(f, "not enough samples to fill buffer"),
-            Error::UnexpectedInputBufferSize(e, p) => write!(f, "unexpected input buffer size {{ expected: {}, produced: {} }}", e, p),
-            Error::UnexpectedOutputBufferSize(e, p) => write!(f, "unexpected output buffer size {{ expected: {}, produced: {} }}", e, p),
+            Error::NumBands => write!(f, "number of bands must be greater than zero"),
+            Error::LowerCutoff => write!(f, "lower cutoff must be greater than zero"),
+            Error::UpperCutoff => write!(f, "upper cutoff must be greater than zero"),
+            Error::CutoffOrder => write!(f, "lower cutoff must be less than upper cutoff"),
+            Error::InputBuffer(e, p) => write!(f, "unexpected input buffer size {{ expected: {}, produced: {} }}", e, p),
+            Error::OutputBuffer(e, p) => write!(f, "unexpected output buffer size {{ expected: {}, produced: {} }}", e, p),
             Error::NumSamples(e, p) => write!(f, "unexpected number of samples {{ expected: {}, produced: {} }}", e, p),
             Error::SamplingRate(s) => write!(f, "sampling rate must be greater than zero and finite {{ found: {} }}", s),
         }
